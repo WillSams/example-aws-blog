7d013a45c778bdb79c7f68a474ff2bbc49a29330
Will Sams <will@samswebs.com>
2022-02-23 08:54:37 -0500

    Update index.js

--- backend/src/utils/server/index.js
+++ backend/src/utils/server/index.js
@@ -1,8 +1,9 @@
 const isDevelopmentHost = () =>
   process.env.NODE_ENV === 'development' ? true : false;
 
-const blogTableName = () =>
-  process?.env?.NODE_ENV ? `Blog-${process?.env?.NODE_ENV}` : process?.env?.SOCCER_TABLE;
+const blogTableName = process?.env?.NODE_ENV 
+  ? `Blog-${process?.env?.NODE_ENV}` 
+  : process?.env?.SOCCER_TABLE;
 
 
 module.exports = {
